89057fa608270c40920a16002d808535eb433961
santii85233 <165356451+santii85233@users.noreply.github.com>
2025-09-29 21:13:32 -0500

    Updated package.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    minify: 'terser',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
